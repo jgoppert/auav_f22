7f266560e43252b6804add215ce57c042f8200b8
James Goppert <james.goppert@gmail.com>
2022-08-15 13:18:14 -0400

    Add scripts.

---
+++ px4_scripts/px4_build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd $HOME/PX4-Autopilot && make distclean && make px4_sitl_rtps
---
+++ px4_scripts/px4_start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd $HOME/PX4-Autopilot/build/px4_sitl_rtps && ./bin/px4 ./etc -s etc/init.d-posix/rcS
