7a482b8eeb1ea8f59303f6e380352247b85aa5af
James Goppert <james.goppert@gmail.com>
2022-08-22 01:48:31 -0400

    Work on PX4 rtps with v1.12.3.

--- onboard/ws/src/auav_f22/launch/sample.launch.py
+++ onboard/ws/src/auav_f22/launch/sample.launch.py
@@ -5,8 +5,19 @@ from launch.actions import ExecuteProcess
 def generate_launch_description():
     return LaunchDescription([
         # MicroDDS agent to translate PX4 msgs to ROS2
+        #ExecuteProcess(
+        #    cmd='MicroXRCEAgent udp4 -p 15555'.split(' '),
+        #    output='log'
+        #),
         ExecuteProcess(
-            cmd='MicroXRCEAgent udp4 -p 15555'.split(' '),
-            output='log'
+            cmd='micrortps_agent -t UDP'.split(' '),
+            output='screen',
+            shell=True,
+        ),
+        Node(
+            package='auav_f22',
+            executable='offboard',
+            name='offboard',
+            output='screen'
         ),
     ])
---
+++ px4/README.md
@@ -0,0 +1,4 @@
+We are using v1.12.3 as there are two issues with v1.13.0:
+
+1. ROS RTPS naming issue
+2. fmu-v5 RTPS breaks
